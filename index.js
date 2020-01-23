3bb79a5f1a67078b35174027433c39f5ac0919aa
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-23 10:03:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says mewo!`
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        if (this.sex = 'male') {
+            return `it's me! ${this.name} the parrot!`;
+        }
+        else {
+            return `${this.name} says squawk!`;
+        }
+    }
+    
+}
